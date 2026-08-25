3ef3fd01e7eb46c1e53bd230d13605b57c9db8e2
Samuel Thomas <samuel.thomas@pomona.edu>
2026-08-25 10:55:46 -0700

    first commit

---
+++ util/string.c
@@ -0,0 +1,31 @@
+#include "util/string.h"
+
+#include <stddef.h> // for NULL
+
+/*
+ * TODO: implement me!
+ */
+long strlen(const char *str) {
+    return 0;
+}
+
+/*
+ * TODO: implement me!
+ */
+char *strstr(const char *haystack, const char *needle) {
+    return NULL;
+}
+
+/*
+ * TODO: implement me!
+ */
+char *strncat(char *dest, const char *src, long n) {
+    return NULL;
+}
+
+/*
+ * TODO: implement me!
+ */
+int strncmp(const char *s1, const char *s2, long n) {
+    return 0;
+}
